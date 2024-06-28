10b16b25f18df1fbd26d64bddb8a32d9b3c13dd4
Karan Bhagnani <karanbhagnani@Karans-Macbook-Pro.local>
2024-06-28 11:13:54 +0530

    Third

--- src/header.jsx
+++ src/header.jsx
@@ -2,7 +2,7 @@ import './Header.css';
 function Header(){
     return(
         <div className="navBar">
-        <img src="public/companyLogo.png" alt="Company logo"/>
+        <img src="https://assets-global.website-files.com/602f6e4385f3c15b5ee4238a/607955a9d5e3528558cc00e1_side_panda-p-500.png" alt="Company logo"/>
         <div className="navItem">
             <div className="item">Menu</div>
             <div className="item">Contact Us</div>
